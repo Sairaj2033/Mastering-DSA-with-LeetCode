58b3a283ed93be4900008e9cc835a78d817ba694
Sairaj Patil <sairajpatil035@gmail.com>
2026-07-20 00:29:48 +0530

    Time: 2 ms (99.30%) | Memory: 47.2 MB (23.34%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        //sairaj
         HashMap<Integer, Integer> map = new HashMap<>();
         
         for (int i = 0; i < nums.length; i++) {
